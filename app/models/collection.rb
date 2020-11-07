88e7941355f25c1a6a891ab781845c1236e58016
RoryMusinskas <rorymuzz@gmail.com>
2020-11-07 16:06:08 +1100

    Added method to create geoJSON object to pass to the index_map.js for marker rendering

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,27 @@
 class Collection < ApplicationRecord
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
+
+  def coordinates
+    [longitude, latitude]
+  end
+
+  def to_feature
+    {
+      "type": 'Feature',
+      "properties": {
+        "collection_id": id,
+        "name_id": name,
+        "description": description,
+        "price": price,
+        "available_until": available_until,
+        "longitude": longitude,
+        "latitude": latitude,
+        "seller_id": seller_id
+      },
+      "geometry": {
+        "type": 'Point',
+        "coordinates": coordinates
+      }
+    }
+  end
 end
