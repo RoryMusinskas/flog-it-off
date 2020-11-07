915c55373f4d4d248fa75a85073ad61ca9573d80
RoryMusinskas <rorymuzz@gmail.com>
2020-11-07 13:06:42 +1100

    changed around which select tag shows first on signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: %i[seller buyer]
+  enum role: %i[buyer seller]
   has_many :collections, foreign_key: 'seller_id', class_name: 'Collection'
   has_many :payments, foreign_key: 'buyer_id', class_name: 'Payment'
 end
