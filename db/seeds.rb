cf913794209fb2fe197ab3075fcccbabeb9d2e2d
RoryMusinskas <rorymuzz@gmail.com>
2020-11-07 20:03:44 +1100

    Added seeds to new Category model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seeding the categories for the app
+Category.create([{ name: 'Doors' }, { name: 'Electrical' }, { name: 'Fixtures' }, { name: 'Flooring' }, { name: 'Masonry' }, { name: 'Plaster' }, { name: 'Plumbing' }, { name: 'Roofing' }, { name: 'Timber' }, { name: 'Windows' }])
