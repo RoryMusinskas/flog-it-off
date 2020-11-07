6add45f5d7aad813600d3be447c1b28060f169f9
RoryMusinskas <rorymuzz@gmail.com>
2020-11-07 16:07:51 +1100

    Formatting on saze

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_015143) do
-
+ActiveRecord::Schema.define(version: 20_201_107_015_143) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "collections", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.float "price"
-    t.integer "quantity"
-    t.datetime "available_until"
-    t.float "longitude"
-    t.float "latitude"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "seller_id"
-    t.integer "available_hours_morning"
-    t.integer "available_hours_night"
+  create_table 'collections', force: :cascade do |t|
+    t.string 'name'
+    t.string 'description'
+    t.float 'price'
+    t.integer 'quantity'
+    t.datetime 'available_until'
+    t.float 'longitude'
+    t.float 'latitude'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'seller_id'
+    t.integer 'available_hours_morning'
+    t.integer 'available_hours_night'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "role"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'role'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
