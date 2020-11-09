4ab91184040431ea3244ade363ca97e11a031fe0
muzz00ka <rorymuzz@gmail.com>
2020-11-09 16:16:43 +1100

    Added first_name, last_name and time_zone columns to the users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_225850) do
+ActiveRecord::Schema.define(version: 2020_11_09_043032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,9 @@ ActiveRecord::Schema.define(version: 2020_11_08_225850) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "role"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "time_zone", default: "UTC"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
