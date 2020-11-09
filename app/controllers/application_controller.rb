a0fd479c0b75d70ac604b72f71c4c947c6726fa0
muzz00ka <rorymuzz@gmail.com>
2020-11-09 16:10:04 +1100

    Added in time zone setting for each user when they sign up. Added in user first and last name fields.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :set_time_zone, if: :current_user
 
   def after_sign_in_path_for(_resoure)
     collections_path
@@ -8,6 +9,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :first_name, :last_name, :time_zone])
+  end
+
+  def set_time_zone(&block)
+    Time.use_zone(current_user.time_zone, &block)
   end
 end
