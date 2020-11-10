87fec832de7d68da8f18e89741556659969b3f29
muzz00ka <rorymuzz@gmail.com>
2020-11-10 18:48:55 +1100

    Changed cancancan logic for when a guest is browsing

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,14 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    can :read, Collection, public: true
+    can :read, Collection
 
-    can :read, Collection if user.buyer?
-    if user.seller?
-      can :read, Collection
-      can :create, Collection
-      can :update, Collection, seller_id: user.id
-      can :destroy, Collection, seller_id: user.id
+    if user.present?
+      if user.seller?
+        can :create, Collection
+        can :update, Collection, seller_id: user.id
+        can :destroy, Collection, seller_id: user.id
+      end
     end
   end
 end
