181a9678ba34444597d261d9d6cd0e8e1e3f3aed
muzz00ka <rorymuzz@gmail.com>
2020-11-10 18:48:30 +1100

    added basic stripe implementation and logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'home#page'
   get 'home/page'
+  get 'payments/success', to: 'payments#success'
+  post '/payments/webhook', to: 'payments#webhook'
   devise_for :users
   resources :collections
-  root to: 'home#page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # each time a post request is made, run the increment method
   resources :collections do
