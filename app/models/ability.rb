29c0f348ef76a7e67cb73aa7260614488e0ea176
RoryMusinskas <rorymuzz@gmail.com>
2020-11-10 11:34:28 +1100

    Adding destroy method, not delete

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,14 +7,12 @@ class Ability
     # Define abilities for the passed in user here. For example:
     can :read, Collection, public: true
 
-    if user.buyer?
-      can :read, Collection
-    end
+    can :read, Collection if user.buyer?
     if user.seller?
       can :read, Collection
       can :create, Collection
       can :update, Collection, seller_id: user.id
-      can :delete, Collection, seller_id: user.id
+      can :destroy, Collection, seller_id: user.id
     end
   end
 end
