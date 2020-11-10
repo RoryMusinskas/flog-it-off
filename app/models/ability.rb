31ea0ba4c93dde065f08ae1c84351046e8c6eb38
muzz00ka <rorymuzz@gmail.com>
2020-11-10 20:36:02 +1100

    removed comment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
     can :read, Collection
 
     if user.present?
