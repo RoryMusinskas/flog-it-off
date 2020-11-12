d90d24419dd81c5c5d5290f367988a60f22f7bf5
RoryMusinskas <rorymuzz@gmail.com>
2020-11-12 14:01:19 +1100

    Added in better logic for the queries from the database for active collections and completed collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -6,14 +6,18 @@ class CollectionsController < ApplicationController
 
   layout '_base'
   # GET /collections
-  # GET /collections.json
   def index
     # collections which aren't expired
-    @collections = Collection.where('available_until  >= ?', Time.now)
-    @latest_collections = @collections.last(10)
+    @collections = Collection.not_expired
+    # collections where payment has been made
+    @completed_collections = @collections.joins(:payment).paid
+    # collections which are not expired and have not been paid for
+    @active_collections = @collections.where.not(id: @completed_collections.ids)
+    # the 10 latest active collections
+    @latest_collections = @active_collections.last(10).reverse
     if can? :update, Collection
-    @user_collections = @collections.where(id: current_user.id)
-    @expired_collections = current_user.collections.where('available_until <= ?', Time.now)
+      @user_collections = @active_collections.where(seller_id: current_user.id)
+      @expired_collections = current_user.collections.where('available_until <= ?', Time.now)
     end
 
     # get the values from the database and convert them to geojson for the map layer
@@ -23,7 +27,6 @@ class CollectionsController < ApplicationController
   end
 
   # GET /collections/1
-  # GET /collections/1.json
   def show
     @category = Category.all
     @available_until = @collection.available_until
@@ -31,27 +34,32 @@ class CollectionsController < ApplicationController
     @time = @available_until.to_formatted_s(:short)
 
     # stripe session implementation
-    session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      customer_email: current_user.email,
-      line_items: [{
-        name: @collection.name,
-        description: @collection.description,
-        images: [@collection.image],
-        amount: (@collection.price * 100).to_i,
-        currency: 'aud',
-        quantity: 1,
-      }],
-      payment_intent_data: {
-        metadata: {
-          collection_id: @collection.id,
-          user_id: current_user.id
-        }
-      },
-      success_url: "#{root_url}payments/success?collectionId=#{@collection.id}",
-      cancel_url: "#{root_url}collections"
-    )
-    @session_id = session.id
+    if @collection.price != 0
+      if user_signed_in?
+        @session = Stripe::Checkout::Session.create(
+          payment_method_types: ['card'],
+          customer_email: current_user.email,
+          line_items: [{
+            name: @collection.name,
+            description: @collection.description,
+            images: [@collection.image],
+            amount: (@collection.price * 100).to_i,
+            currency: 'aud',
+            quantity: 1
+          }],
+          payment_intent_data: {
+            metadata: {
+              collection_id: @collection.id,
+              buyer_id: current_user.id,
+              seller_id: @collection.seller_id
+            }
+          },
+          success_url: "#{root_url}payments/success?collectionId=#{@collection.id}",
+          cancel_url: "#{root_url}collections"
+        )
+        @session_id = @session.id
+      end
+    end
   end
 
   # GET /collections/new
@@ -66,42 +74,34 @@ class CollectionsController < ApplicationController
   end
 
   # POST /collections
-  # POST /collections.json
   def create
     @collection = current_user.collections.new(collection_params)
 
     respond_to do |format|
       if @collection.save
         format.html { redirect_to @collection, notice: 'Collection was successfully created.' }
-        format.json { render :show, status: :created, location: @collection }
       else
         format.html { render :new }
-        format.json { render json: @collection.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /collections/1
-  # PATCH/PUT /collections/1.json
   def update
     respond_to do |format|
       if @collection.update(collection_params)
         format.html { redirect_to @collection, notice: 'Collection was successfully updated.' }
-        format.json { render :show, status: :ok, location: @collection }
       else
         format.html { render :edit }
-        format.json { render json: @collection.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /collections/1
-  # DELETE /collections/1.json
   def destroy
     @collection.destroy
     respond_to do |format|
       format.html { redirect_to collections_url, notice: 'Collection was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -120,7 +120,7 @@ class CollectionsController < ApplicationController
   def build_geojson
     {
       type: 'FeatureCollection',
-      features: @collections.map(&:to_feature)
+      features: @active_collections.map(&:to_feature)
     }
   end
 
