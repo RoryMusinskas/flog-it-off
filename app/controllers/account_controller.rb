e99f09bebeb95e56576c82ad0ad36fd967be94a8
RoryMusinskas <rorymuzz@gmail.com>
2020-11-12 20:47:08 +1100

    Query the database to show each user their sold and bought items in their profile page

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,5 +1,8 @@
 class AccountController < ApplicationController
   prepend_view_path(File.join(Rails.root, 'app/views/account/'))
   layout 'application'
-  def profile; end
+  def profile
+    @seller_completed_collections = Payment.where(seller_id: current_user.id)
+    @buyer_completed_collections = Payment.where(buyer_id: current_user.id)
+  end
 end
