cfb6859dd6270d19296e0436935e9d4aead85217
RoryMusinskas <rorymuzz@gmail.com>
2020-11-12 13:59:13 +1100

    Added in allowed params for account update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   around_action :set_time_zone, if: :current_user
 
-  def after_sign_in_path_for(_resoure)
+  def after_sign_in_path_for(_resource)
     collections_path
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :first_name, :last_name, :time_zone])
+    # devise params for sign up
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[role first_name last_name time_zone])
+    # devise params for updating account
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[role first_name last_name time_zone])
   end
 
   def set_time_zone(&block)
