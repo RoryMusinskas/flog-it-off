80e2de86f087d06c113b457263034e37551c2031
RoryMusinskas <rorymuzz@gmail.com>
2020-11-12 14:01:58 +1100

    Added metadata for more detail in stripe payment.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,17 +1,16 @@
 class PaymentsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:webhook]
 
-  def success
-  end
-
   def webhook
     payment_id = params[:data][:object][:payment_intent]
     payment = Stripe::PaymentIntent.retrieve(payment_id)
     collection_id = payment.metadata.collection_id
-    user_id = payment.metadata.user_id
-    p "Event id " + collection_id
-    p "User id " + user_id
-    render plain: "Success"
+    buyer_id = payment.metadata.buyer_id
+    seller_id = payment.metadata.seller_id
+    amount_total = params[:data][:object][:amount_total]
+    currency = params[:data][:object][:currency]
+    payment_status = params[:data][:object][:payment_status]
+    render plain: 'Success'
+    Payment.create collection_id: collection_id, buyer_id: buyer_id, seller_id: seller_id, amount_total: amount_total, currency: currency, payment_status: payment_status
   end
 end
-
