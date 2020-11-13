c9ffac26b52e7f5abad4e3b6231483fd4009f1ea
RoryMusinskas <rorymuzz@gmail.com>
2020-11-13 18:06:15 +1100

    Adding suggested fix for heroku emal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  # Rails.application.routes.default_url_options[:host] = '???'
+  Rails.application.routes.default_url_options[:host] = '???'
   ActionMailer::Base.smtp_settings = {
     user_name: 'apikey',
     password: 'Rails.application.credentials.dig(:sendgrid, :api_key)',
