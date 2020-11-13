7d824eeeccd4a6bdcb9653f15e81cb37dd4c790d
RoryMusinskas <rorymuzz@gmail.com>
2020-11-13 15:18:34 +1100

    Added extra logic for searching the database and adding to map. Logic added for destination on stripe checkout, so the money goes to the seller not my account.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -7,22 +7,24 @@ class CollectionsController < ApplicationController
   layout '_base'
   # GET /collections
   def index
-    # All collections
-    # @collections = Collection.all
+    # All collections made
+    @collections = Collection.all
+    # All payments made
+    @payments = Payment.all
     # All collections which aren't expired or aren't sold, for the index map
-    @all_active_unsold_collections = Collection.joins(:payment).where('payments.collection_id': @collections.ids).not_expired
+    @all_active_unsold_collections = Collection.where.not(id: Payment.pluck(:collection_id)).not_expired
     # All the collections which belong to the current user
     @current_user_collections = Collection.where(seller_id: current_user.id)
     # All the collections which the current user has sold
     @current_user_sold_collections = Collection.joins(:payment).where('payments.seller_id': current_user.id)
     # The last 10 collections in the database
-    @latest_collections = Collection.where('available_until >= ?', Time.now).last(10).reverse
+    @latest_collections = @all_active_unsold_collections.last(10).reverse
 
     if can? :update, Collection
       # All the current users active collections, which are not sold and are not expired
       @active_collections = @current_user_collections.where.not(id: @current_user_sold_collections.ids).where('available_until >= ?', Time.now)
       # All the current users collections, which have expired but have not been sold
-      @expired_unpaid_collections = @current_user_collections.where.not(id: @current_user_sold_collections.ids).where('available_until <= ?', Time.now)
+      @expired_unpaid_collections = @current_user_collections.where.not(id: @payments).where('available_until <= ?', Time.now)
     end
 
     # get the values from the database and convert them to geojson for the map layer
@@ -33,10 +35,15 @@ class CollectionsController < ApplicationController
 
   # GET /collections/1
   def show
+    # Get all the seeded categories from the DB
     @category = Category.all
+    # Get the available until data for the collection
     @available_until = @collection.available_until
     # format the available until datetime to the short style for better readability
     @time = @available_until.to_formatted_s(:short)
+    # Seller stripe account, this is used for the destination. Get the seller id from the collection, then get the first (as it return an active record association), then get the sellers stripe id.
+    @seller_stripe_id = User.where(id: @collection.seller_id).first.stripe_user_id
+    @payments = Payment.all
 
     # stripe session implementation
     if @collection.price != 0
@@ -57,6 +64,9 @@ class CollectionsController < ApplicationController
               collection_id: @collection.id,
               buyer_id: current_user.id,
               seller_id: @collection.seller_id
+            },
+            transfer_data: {
+              destination: @seller_stripe_id.to_s
             }
           },
           success_url: "#{root_url}payments/success?collectionId=#{@collection.id}",
@@ -125,7 +135,7 @@ class CollectionsController < ApplicationController
   def build_geojson
     {
       type: 'FeatureCollection',
-      features: @collections.map(&:to_feature)
+      features: @all_active_unsold_collections.map(&:to_feature)
     }
   end
 
