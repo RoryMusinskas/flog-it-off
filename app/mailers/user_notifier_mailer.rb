31a74018f50861267e2eb639e9fdf31c422c25bf
RoryMusinskas <rorymuzz@gmail.com>
2020-11-14 13:10:58 +1100

    New mailer method for when a new collection is made

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,8 +1,13 @@
 class UserNotifierMailer < ApplicationMailer
   default from: 'devrorym@gmail.com'
   # send a signup email to the user, pass in the user object that   contains the user's email address
-  def send_signup_email(_user)
+  def send_signup_email
     @user = current_user
     mail(to: @user.email, subject: 'Thanks for signing up to flog it off')
+  end
+
+  def send_collection_new_mail(user)
+    @user = user
+    mail(to: @user.email, subject: 'Your collection has been created')
   end
 end
