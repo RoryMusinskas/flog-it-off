7bb6f87e0612084f46e85af79456258fad0272c0
RoryMusinskas <rorymuzz@gmail.com>
2020-11-15 18:13:10 +1100

    Updated schema after deleting the address table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_095140) do
+ActiveRecord::Schema.define(version: 2020_11_15_021821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,16 +34,6 @@ ActiveRecord::Schema.define(version: 2020_11_11_095140) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "addresses", force: :cascade do |t|
-    t.bigint "collection_id", null: false
-    t.string "address"
-    t.string "city"
-    t.string "state"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["collection_id"], name: "index_addresses_on_collection_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -75,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_11_11_095140) do
     t.integer "available_hours_morning"
     t.integer "available_hours_night"
     t.integer "visit_count", default: 0
+    t.string "address"
   end
 
   create_table "payments", force: :cascade do |t|
@@ -107,7 +98,6 @@ ActiveRecord::Schema.define(version: 2020_11_11_095140) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "addresses", "collections"
   add_foreign_key "collection_categories", "categories"
   add_foreign_key "collection_categories", "collections"
   add_foreign_key "payments", "collections"
