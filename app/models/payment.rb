8c0fc2086fb78bea06294de13dedf632ee13e757
RoryMusinskas <rorymuzz@gmail.com>
2020-11-15 18:10:23 +1100

    Uncommented the payment belongs to

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,5 @@
 class Payment < ApplicationRecord
   belongs_to :collection
   belongs_to :buyer, foreign_key: 'buyer_id', class_name: 'User'
-  # belongs_to :seller, foreign_key: 'seller_id', class_name: 'User'
+  belongs_to :seller, foreign_key: 'seller_id', class_name: 'User'
 end
