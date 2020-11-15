f5560bb6981f2e3fd6078ed820c70e39a6f684b8
RoryMusinskas <rorymuzz@gmail.com>
2020-11-15 18:10:51 +1100

    Added in validations for the User model, on top of devise inbuilt validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
   enum role: %i[buyer seller]
   has_many :collections, foreign_key: 'seller_id', class_name: 'Collection'
   has_many :payments, foreign_key: 'buyer_id', class_name: 'Payment'
+
+  # Validations
+  validates :email, presence: true
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :time_zone, presence: true
+  validates :role, presence: true
 end
